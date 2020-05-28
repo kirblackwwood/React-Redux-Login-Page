cfce2a07b5387a0459c21474f1561ff0fc05e42d
kirblackwwood <matiashcalgary@gmail.com>
2020-05-28 16:03:52 +0300

    Nice Epic Commit

---
+++ src/App/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { history } from '../_helpers';
+import { alertActions } from '../_actions';
+import { PrivateRoute } from '../_components';
+import { HomePage } from '../HomePage';
+import { LoginPage } from '../LoginPage';
+import { RegisterPage } from '../RegisterPage';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        history.listen((location, action) => {
+
+            this.props.clearAlerts();
+        });
+    }
+
+    render() {
+        const { alert } = this.props;
+        return (
+
+            <div style={{color: "grey", backgroundImage: "linear-gradient(to right, #00ff80, #6600cc)",
+            height: "680px"}} className="jumbotron">
+            <div>
+    
+     </div>
+               <div className="container" style={{color: "purple"}}>
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <Switch>
+                                <PrivateRoute exact path="/" component={HomePage} />
+                                <Route path="/login" component={LoginPage} />
+                                <Route path="/register" component={RegisterPage} />
+                                <Redirect from="*" to="/" />
+                            </Switch>
+                        </Router>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+function mapState(state) {
+    const { alert } = state;
+    return { alert };
+}
+
+const actionCreators = {
+    clearAlerts: alertActions.clear
+};
+
+const connectedApp = connect(mapState, actionCreators)(App);
+export { connectedApp as App };
