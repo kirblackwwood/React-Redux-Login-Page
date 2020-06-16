40b048e43dab580a3ad9b2be7c254dd35a813360
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-17 01:30:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.render('index.js');
 });
 
 app.listen(port, function() {
