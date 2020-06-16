a99c0d26dbd043391d761083d2da4e44b2b7c48a
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-17 01:50:49 +0300

    Update App.jsx

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -8,23 +8,9 @@ import { HomePage } from '../HomePage';
 import { LoginPage } from '../LoginPage';
 import { RegisterPage } from '../RegisterPage';
 
-var express = require('express');
-var app = express();
 
 var port = process.env.PORT || 8080;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
 
 class App extends React.Component {
     constructor(props) {
