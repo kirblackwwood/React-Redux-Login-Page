f70f05ca919046033a8ac337000dbde25d201339
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-17 01:22:09 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 var express = require('express');
-
 var app = express();
-const PORT = process.env.PORT || 3000;
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.use(function(req, res, next) {
-    if(req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
-        next();
-    }
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-app.use(express.static('public'))
-
-app.get('/', function (req, res) {
-   res.send("Hello World!"); 
-});
-
-app.listen(PORT, function(){
-    console.log(`Listen on port ${PORT}...`);
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
 });
