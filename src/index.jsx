38fa4791be9b98c579ef800817024c55c137bff0
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-17 01:44:02 +0300

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-
 import { store } from './_helpers';
 import { App } from './App';
 import { configureFakeBackend } from './_helpers';
 configureFakeBackend();
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var port = process.env.PORT || 8080;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/public'));
 
 render(
