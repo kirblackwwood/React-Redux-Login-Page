d6b3fd81e808b958bff07be83bbbe43b30d527e5
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-17 01:08:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,9 +14,7 @@ app.use(function(req, res, next) {
 
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-   res.send("Hello World!"); 
-});
+
 
 app.listen(PORT, function(){
     console.log(`Listen on port ${PORT}...`);
