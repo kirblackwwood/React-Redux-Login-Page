d324331203797cf6b891ef14cd639b99d090602d
kirblackwwood <49650659+kirblackwwood@users.noreply.github.com>
2020-06-19 00:38:11 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,7 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
 
 module.exports = {
